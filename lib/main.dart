b5bb992e94ca37240398b4de23b443f51df9b7e8
Debmalya99 <debmalyamitra99@gmail.com>
2020-08-20 23:36:26 +0530

    Add button adding pregenerated events

--- lib/main.dart
+++ lib/main.dart
@@ -21,20 +21,14 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoApp extends State<TodoApp> {
+  List<String> _todoItems = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("To Do"),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: <Widget>[
-          _listItem("This is sample event 1"),
-          _listItem("This is sample event 2"),
-          _listItem("This is sample event 3"),
-        ],
-      ),
+      body: _buildTodoList(),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -44,22 +38,42 @@ class _TodoApp extends State<TodoApp> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: _addTodoItem,
         child: const Icon(Icons.add),
         tooltip: "Add event",
       ),
     );
   }
-  Widget _listItem(String text){
-    bool checkedValue = false;
-    return CheckboxListTile(
-      title : Text(text),
-      value : checkedValue,
-      onChanged : (bool new_value){
-        setState((){
-          checkedValue = new_value;
-        });
-      },
+
+  void _addTodoItem() {
+    /*
+    What this function does: This function simply adds a pregenerated todo item.
+    But it does not care about any interface changes etc. Its job is to append the array that's all
+     */
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Event ' + index.toString());
+    });
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(itemBuilder: (context, index) {
+      // What this itemBuilder does in my opinion:
+      /*
+        I think, it means itemBuilder takes as input a function, which we give here
+        as an anonymous lambda, as how the individual list items will be built.
+         */
+      if (index < _todoItems.length) {
+        return _buildTodoItem(_todoItems[index]);
+      }
+    });
+  }
+
+  Widget _buildTodoItem(String todoItem) {
+    return Card(
+      child: ListTile(
+        title: Text(todoItem),
+      ),
     );
   }
 }
